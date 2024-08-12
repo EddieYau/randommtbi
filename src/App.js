2cd0aa35180bbbb82567d05914925b82be4242f3
Yau Eddie <yaueddie@MacBook-Air-2020.local>
2024-08-12 23:38:36 +0800

    initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+  
+  return (
+    <div className="App">
+      <h1>mbti without questioning</h1>
+      <button >Click here</button>
+      {}
+    </div>
+  );
+}
+
+export default App;
